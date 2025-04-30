ad44d508ee109643ea2e621603c3e2be40a33f6f
Jorch34 <jorchgt34@gmail.com>
2025-04-29 22:49:18 -0600

    Clases y objetos creados en un archivo diferente usando namespace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CoreEscuela.Entidades;
+
+namespace Etapa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var escuela = new Escuela("uaa", 1980);
+            escuela.País = "México";
+            escuela.Ciudad = "Aguascalientes";
+            Console.WriteLine(escuela.Nombre);
+            Console.WriteLine(escuela.País);
+        }
+    }
+}
