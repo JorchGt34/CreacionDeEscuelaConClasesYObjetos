52806f670c35cfb33c3251f53a52021b044c39bf
Jorch34 <jorchgt34@gmail.com>
2025-05-02 19:54:45 -0600

    Uso de arreglos y funciones que utilizan los ciclos For, While, Do-While, ForEach

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Timers;
 using CoreEscuela.Entidades;
 using Etapa1.Entidades;
 
@@ -17,22 +19,74 @@ namespace Etapa1
             Console.WriteLine(escuela);
             var escuelaDos = new Escuela("Platzi Academy", 2012, TiposDeEscuela.Preparatoria, ciudad: "Bogotá");//Los parametros opcionales pueden ser omitidos al poner el siguiente parametro seguudo de ":", o al no poner más parametros
             //Si no hay constructor, se pueden agregar valores de la siguiente manera
-            var curso1 = new Curso() {
+
+            //Arreglos
+            var arregloCursos = new Curso[3]; //Aqui se registra un arreglo de 3 posiciones que contiene 3 datos de tipo curso
+
+            //Aqui se le asigna el valor del curso 1 a la posicion uno del arreglo
+            var curso1 = new Curso()
+            {
                 Nombre = "101",
                 //UniqueId = "jajajaja" Esto no se puede cuando la propiedad "set" es privada
             };
-            var curso2 = new Curso() {
+
+            arregloCursos[0] = curso1;
+            var curso2 = new Curso()
+            {
                 Nombre = "201"
             };
-            var curso3 = new Curso() {
+            arregloCursos[1] = curso2;
+            var curso3 = new Curso()
+            {
                 Nombre = "301"
             };
+            arregloCursos[2] = curso3;
 
-            Console.WriteLine(escuela);
-            System.Console.WriteLine("===========");
-            Console.WriteLine(curso1.Nombre + "," + curso1.UniqueId);
-            Console.WriteLine(curso2.Nombre + "," + curso2.UniqueId);
-            Console.WriteLine(curso3);
+            Console.WriteLine("\n===============\n");
+            ImprimirCursosFor(arregloCursos);
+            Console.WriteLine("\n===============\n");
+            ImprimirCursosWhile(arregloCursos);
+            Console.WriteLine("\n===============\n");
+            ImprimirCursosDoWhile(arregloCursos);
+            Console.WriteLine("\n===============\n");
+            ImprimirCursosForEach(arregloCursos);
+        }
+        //Uso de cilcos para imprimir el arreglo de objetos
+        private static void ImprimirCursosFor(Curso[] arregloCursos)
+        {
+            Console.WriteLine("\nImprimir el arreglo de objetos con un ciclo for->");
+            for (int i = 0; i < arregloCursos.Length; i++)
+            {
+                Console.WriteLine("El nombre del curso es: " + arregloCursos[i].Nombre + ".\nY el ID unico del curso es: " + arregloCursos[i].UniqueId);
             }
+        }
+        private static void ImprimirCursosWhile(Curso[] arregloCursos)
+        {
+            Console.WriteLine("\nImprimir el arreglo de objetos con un ciclo While->");
+            int contador = 0;
+            while (contador < arregloCursos.Length)
+            {
+                Console.WriteLine("El nombre del curso es: " + arregloCursos[contador].Nombre + ".\nY el ID unico del curso es: " + arregloCursos[contador].UniqueId);
+                contador++;
+            }
+        }
+        private static void ImprimirCursosDoWhile(Curso[] arregloCursos)
+        {
+            Console.WriteLine("\nImprimir el arreglo con un ciclo Do-While->");
+            int contador = 0;
+            do
+            {
+                Console.WriteLine("El nombre del curso es: " + arregloCursos[contador].Nombre + ".\nY el ID unico del curso es: " + arregloCursos[contador].UniqueId);
+                contador++;
+            } while (contador < arregloCursos.Length);
+
+        }
+        private static void ImprimirCursosForEach(Curso[] arregloCursos)
+        {
+            foreach (var curso in arregloCursos)
+            {
+                Console.WriteLine("El nombre del curso es: " + curso.Nombre + ".\nY el ID unico del curso es: " + curso.UniqueId);
+            }
+        }
     }
 }
--- obj/Debug/net9.0/Etapa1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Etapa1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c018c894b49e98ba1a838535c141fc2a3660a5e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbcf2743e6f559ad55c535f8a5e12dc1ad93da24")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
