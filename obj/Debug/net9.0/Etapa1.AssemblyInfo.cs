99f467835b41a33872eed0ffa0a636d650c3b1ff
Jorch34 <jorchgt34@gmail.com>
2025-05-06 20:48:32 -0600

    Fin de configuracion de repositorio remoto

--- obj/Debug/net9.0/Etapa1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Etapa1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34b5be3d67bb21cc67f830dcac2d1e6caf03ea0b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70eef90b0416ee1596fd13cef1586823513b7ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Etapa1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
